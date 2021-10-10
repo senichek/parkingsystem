826412318692025949dde90175413dd396613f7e
Olexiy <senichek@gmail.com>
2021-10-10 15:38:46 +0200

    testParkingACar and testParkingLotExit were compl.

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,9 +1,11 @@
 package com.parkit.parkingsystem.integration;
 
+import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
 import org.junit.jupiter.api.AfterAll;
@@ -14,6 +16,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ParkingDataBaseIT {
@@ -51,7 +54,24 @@ public class ParkingDataBaseIT {
     public void testParkingACar(){
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processIncomingVehicle();
-        //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
+        // TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
+
+        /*
+         * 1. If a ticket is created, it will have ID=1, parking_number=1 and
+         * vehicle_reg_num= ABCDEF. These are the values we can check for when we
+         * retrieve the ticket from DB.
+         * 
+         * 2. If the 1st car has been parked (here we work only with the 1st car), it means 
+         * that the parking_number=1 has been taken by this car and the next available 
+         * parking slot will be parking_number=2.
+         */
+
+        Ticket fromDB = ticketDAO.getTicket("ABCDEF");
+
+        assertEquals(1, fromDB.getId());
+        assertEquals(1, fromDB.getParkingSpot().getId());
+        assertEquals("ABCDEF", fromDB.getVehicleRegNumber());
+        assertEquals(2, parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR));
     }
 
     @Test
@@ -59,7 +79,17 @@ public class ParkingDataBaseIT {
         testParkingACar();
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processExitingVehicle();
-        //TODO: check that the fare generated and out time are populated correctly in the database
-    }
+        // TODO: check that the fare generated and out time are populated correctly in the database
 
+        /*
+         * If the car's exist was processed correctly, the car's ticket will be
+         * generated and its fields will not be null and the parking_number=1
+         * will be available again.
+         * 
+         */
+        Ticket fromDB = ticketDAO.getTicket("ABCDEF");
+        assertNotNull(fromDB.getOutTime(), "a DATE was expected but it was not returned;");
+        assertNotNull(fromDB.getPrice(), "a double was expected but it was not returned;");
+        assertEquals(1, parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR));
+    }
 }
